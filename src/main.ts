e8c64d82100e1da314f47619df7fb4d7d5a92d2d
Manuel <160016796+Manuel-agd@users.noreply.github.com>
2024-06-18 16:59:21 +0200

    Juego 7 y medio

--- src/main.ts
+++ src/main.ts
@@ -1,65 +1,90 @@
-const estiloartista = "background: green; font-size: 20px; font-weight: bold";
+let puntuacion = 0;
 
-interface Grupomusical {
-    artista: string;
-    fundacion: number;
-    activo: boolean;
-    genero: string;
+function muestraPuntuacion(): void {
+    const puntuacionDiv = document.getElementById("puntuacion") as HTMLElement;
+    puntuacionDiv.textContent = `Puntuación: ${puntuacion}`; 
 };
 
-const grupo1 : Grupomusical = {
-    artista: "The Beatles",
-    fundacion: 1960,
-    activo: true,
-    genero: "Pop Rock",
-};
+function pideCartas(): number {
+    let carta = Math.floor(Math.random() * 10) +1;
+    if (carta > 7) carta +=2;
+    return carta;
+}
 
-console.log(`%c${grupo1.artista}`, estiloartista);
-console.log("Año de fundación:", grupo1.fundacion);
-console.log("En activo:", grupo1.activo);
-console.log("Género musical:", grupo1.genero);
+const btnPedirCartas = document.getElementById("pedirCartas") as HTMLButtonElement;
+btnPedirCartas.addEventListener("click", () => {
+    const carta = pideCartas();
+    console.log(`Carta pedida: ${carta}`)
+})
 
-const grupo2 : Grupomusical = {
-    artista: "Queen",
-    fundacion: 1970,
-    activo: false,
-    genero: "Rock",
-};
-console.log(`%c${grupo2.artista}`, estiloartista);
-console.log("Año de fundación:", grupo2.fundacion);
-console.log("En activo:", grupo2.activo);
-console.log("Género musical:", grupo2.genero);
+function muestraCarta (carta: number): void {
+    const cartaImg = document.getElementById("carta") as HTMLImageElement;
+    const cartaURL = `/src/imagenes/${carta}.jpg`;
+    cartaImg.src = cartaURL;
+}
 
-const grupo3 : Grupomusical = {
-    artista: "ACDC",
-    fundacion: 1973,
-    activo: true,
-    genero: "Hard Rock",
-};
-console.log(`%c${grupo3.artista}`, estiloartista);
-console.log("Año de fundación:", grupo3.fundacion);
-console.log("En activo:", grupo3.activo);
-console.log("Género musical:", grupo3.genero);
+btnPedirCartas.addEventListener("click" ,() => {
+    const carta = pideCartas();
+    console.log(`Carta pedida: ${carta}`);
+    muestraCarta(carta);
+    puntuacion += valorCarta(carta);
+    muestraPuntuacion();
+    gameOver();
+})
 
-const grupo4 : Grupomusical = {
-    artista: "Ludwig Van Beethoven",
-    fundacion: 1770,
-    activo: false,
-    genero: "Clasica",
-};
-console.log(`%c${grupo4.artista}`, estiloartista);
-console.log("Año de fundación:", grupo4.fundacion);
-console.log("En activo:", grupo4.activo);
-console.log("Género musical:", grupo4.genero);
+function valorCarta(carta:number): number {
+    if (carta >= 10) return 0.5;
+    return carta;
+}
+/*btnPedirCartas.addEventListener("click", ()=>{
+    const carta = pideCartas();
+    console.log(`Carta pedida: ${carta}`);
+    muestraCarta(carta);
+    puntuacion += valorCarta(carta);
+    muestraPuntuacion();
+});*/
 
-const grupo5 : Grupomusical = {
-    artista: "The Rolling Stones",
-    fundacion: 1962,
-    activo: true,
-    genero: "Rock",
-};
-console.log(`%c${grupo5.artista}`, estiloartista);
-console.log("Año de fundación:", grupo5.fundacion);
-console.log("En activo:", grupo5.activo);
-console.log("Género musical:", grupo5.genero);
+function gameOver(): void{
+    const mensajeDiv = document.getElementById("mensaje") as HTMLElement;
+    if(puntuacion > 7.5) {
+        mensajeDiv.textContent = "Game Over! Te has pasado de 7.5 puntos";
+        btnPedirCartas.disabled = true;
+        btnPlantarse.disabled = true;
+        btnNuevaPartida.style.display = 'block';
+    }
+}
+
+const btnPlantarse = document.getElementById("plantarse") as HTMLButtonElement;
+btnPlantarse.addEventListener("click", () =>{
+    const mensajeDiv = document.getElementById("mensaje") as HTMLElement;
+    btnPedirCartas.disabled = true;
+    btnPlantarse.disabled = true;
+    btnNuevaPartida.style.display = 'block';
+
+    if (puntuacion < 4) {
+        mensajeDiv.textContent = "Has sido muy conservador";
+    }
+    else if (puntuacion === 5){
+        mensajeDiv.textContent = "Te ha entrado canguelo eh";
+    }
+    else if (puntuacion === 6 || puntuacion === 7){
+        mensajeDiv.textContent = "Casi, casi..."
+    }
+    else if (puntuacion === 7.5){
+        mensajeDiv.textContent = "¡Lo has clavado! ¡Enhorabuena!";
+    }
+});
+
+const btnNuevaPartida = document.getElementById("nuevaPartida") as HTMLButtonElement;
+btnNuevaPartida.addEventListener("click", () => {
+    puntuacion = 0;
+    muestraPuntuacion();
+    const cartaImg = document.getElementById("carta") as HTMLImageElement;
+    cartaImg.src = "/src/imagenes/back.jpg";
+    const mensajeDiv = document.getElementById("mensaje") as HTMLElement;
+    mensajeDiv.textContent = "";
+    btnPedirCartas.disabled = false;
+    btnPlantarse.disabled = false;
+    btnNuevaPartida.style.display = "none";
+});
 
